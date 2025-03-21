6ea4e70396a73d23ee79af56037726743f3f6bf5
Priyanshu Kumar <bgmiunban>
2025-03-21 21:27:40 +0530

    Frontend Files added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  },
   plugins: [react()],
   resolve: {
     alias: {
