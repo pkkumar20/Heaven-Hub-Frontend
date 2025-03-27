826aa4c48aca077081175447e61514241793b475
Priyanshu Kumar <bgmiunban>
2025-03-27 10:35:38 +0530

    frontend Files updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port:3000
+  },
   base: "/", // ✅ Ensures correct base URL
   build: {
     outDir: "dist", // ✅ Ensures output in `dist/`
